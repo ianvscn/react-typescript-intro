e349348d8579b70fb59a764fb6a959b35614a8ea
Ian Vasconcelos <99202627+ianvscn@users.noreply.github.com>
2022-12-26 17:41:31 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { Card } from './components/Card'
+import { Layout } from './components/Layout';
+
+function App() {
+  return (
+    <Layout>
+        Hello World
+        <Card 
+          id={1}
+          paragraph='Typescript'
+          details='TS para frontend e backend'
+        />
+        <Card 
+          id={2}
+          paragraph='HTML'
+          details='HTML para frontend'
+        />
+        <Card 
+          id={3}
+          paragraph='SQL'
+          details='SQL para banco de dados'
+        />
+    </Layout>
+  );
+}
+
+export default App;
